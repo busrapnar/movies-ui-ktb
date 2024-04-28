2c862d631e3ff044a65f983c61d0a17f9d1f33e5
busrapinar <bsrapnrr@gmail.com>
2024-04-28 18:11:22 +0300

    poster_path type edited

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,7 +1,7 @@
 export interface Movie {
     id: number;
     title: string;
-    poster_path: string | null;
+    poster_path: string ;
     vote_average: number;
     vote_count: number;
     overview: string;
